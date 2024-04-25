f2aeb795f36c136ac417a720a3867082de9c8d72
AneezAmja <aneez.amja@gmail.com>
2024-04-25 01:39:39 +0100

    fixed positioning issues and created stat bar logic

--- lib/poke_detailed.dart
+++ lib/poke_detailed.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:palette_generator/palette_generator.dart';
 import 'package:test_app/StatBar.dart';
 import 'package:test_app/pokedex.dart';
+import 'package:test_app/util/extensions.dart';
 
 class PokeInfo extends StatefulWidget {
   final Pokemon pokemon;
@@ -54,6 +56,7 @@ class _PokeInfoState extends State<PokeInfo> {
       ),
       body: Stack(
         children: [
+          /* Pokemon top portion with name, types */
           Container(
             width: double.infinity,
             height: double.infinity,
@@ -79,7 +82,7 @@ class _PokeInfoState extends State<PokeInfo> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    widget.pokemon.name,
+                    widget.pokemon.name.capitalize(),
                     style: const TextStyle(
                       fontSize: 32,
                       fontFamily: 'Inter',
@@ -103,7 +106,7 @@ class _PokeInfoState extends State<PokeInfo> {
                               color: Colors.grey[300],
                             ),
                             child: Text(
-                              type.name,
+                              type.name.capitalize(),
                               textAlign: TextAlign.center,
                               style: const TextStyle(fontSize: 10),
                             ),
@@ -115,18 +118,14 @@ class _PokeInfoState extends State<PokeInfo> {
               ),
             ),
           ),
+/*------------------------------------------------------------------------------------------------------------- */
           /* Description section */
           Positioned(
-            top: MediaQuery.of(context).size.height / 2,
+            top: (MediaQuery.of(context).size.height / 2) - 100,
             left: 0,
             right: 0,
             bottom: 0,
-            child:
-
-                // SingleChildScrollView(
-                //   child:
-
-                Container(
+            child: Container(
               decoration: const BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -135,86 +134,100 @@ class _PokeInfoState extends State<PokeInfo> {
                 ),
               ),
               padding: const EdgeInsets.only(top: 55.0, left: 55.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(right: 30.0),
-                        child: Text(
-                          'Weight: ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 30.0),
+                          child: Text(
+                            'Weight: ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
                           ),
                         ),
-                      ),
-                      Text(
-                        '${widget.pokemon.weight} Kg',
-                        style: const TextStyle(fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(right: 30.0),
-                        child: Text(
-                          'Height: ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
+                        Text(
+                          '${widget.pokemon.weight} Kg',
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 30.0),
+                          child: Text(
+                            'Height: ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
                           ),
                         ),
-                      ),
-                      Text(
-                        '${widget.pokemon.height} CM',
-                        style: const TextStyle(fontSize: 16.0),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(right: 20.0),
-                        child: Text(
-                          'Type: ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16.0,
+                        Text(
+                          '${widget.pokemon.height} CM',
+                          style: const TextStyle(fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 20.0),
+                          child: Text(
+                            'Abilities: ',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16.0,
+                            ),
                           ),
                         ),
-                      ),
-                      _getAbilities(),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      _getBaseStats(),
-                    ],
-                  )
-                ],
+                        _getAbilities(),
+                      ],
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 7),
+                          child: Text(
+                            "Stats",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black45,
+                            ),
+                          ),
+                        ),
+                        _getBaseStats(),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
-            // ),
           ),
-          Positioned(
-            top: (MediaQuery.of(context).size.height / 2) - 150,
-            left: 0,
-            right: 0,
-            child: Hero(
-              tag: widget.pokemon.sprite,
-              child: Container(
-                height: 250,
-                width: 250,
-                margin: const EdgeInsets.only(bottom: 40),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10.0),
-                  image: DecorationImage(
-                    fit: BoxFit.contain,
-                    image: NetworkImage(widget.pokemon.sprite),
-                  ),
+          /*------------------------------------------------------------------------------------------------------------- */
+          /* Pokemon hero image */
+          Hero(
+            tag: widget.pokemon.sprite,
+            child: Container(
+              height: 190,
+              width: 190,
+              margin: EdgeInsets.only(
+                top: (MediaQuery.of(context).size.height / 3) - 90,
+                left: (MediaQuery.of(context).size.width - 200) / 2,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: NetworkImage(widget.pokemon.sprite),
                 ),
               ),
             ),
@@ -230,7 +243,7 @@ class _PokeInfoState extends State<PokeInfo> {
       children: widget.pokemon.abilities
           .map(
             (ability) => Text(
-              ability.name,
+              ability.name.capitalize(),
             ),
           )
           .toList(),
@@ -238,29 +251,35 @@ class _PokeInfoState extends State<PokeInfo> {
   }
 
   Widget _getBaseStats() {
-    return Column(children: [
-      const Text(
-        "Stats",
-        textAlign: TextAlign.left,
-        style: TextStyle(
-          fontSize: 22,
-          fontFamily: 'Inter',
-          fontWeight: FontWeight.bold,
-          color: Colors.black45,
-        ),
-      ),
-      const SizedBox(height: 20),
-      Column(
-        // spacing: 4.0,
-        children: widget.pokemon.stats
-            .map(
-              (stat) => StatBar(
-                  label: stat.statName,
-                  value: stat.baseStat,
-                  barColour: _paletteGenerator?.lightVibrantColor?.color),
-            )
-            .toList(),
-      )
-    ]);
+    final List<Widget> statBars = widget.pokemon.stats.map((stat) {
+      return StatBar(
+        label: stat.statName.capitalize(),
+        value: stat.baseStat,
+        barColour: _paletteGenerator?.lightVibrantColor?.color,
+      );
+    }).toList();
+
+    final List<Widget> statRows = [];
+
+    for (int i = 0; i < statBars.length; i += 2) {
+      if (i + 1 < statBars.length) {
+        statRows.add(Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  right: 36.0), // Adjust spacing as needed
+              child: statBars[i],
+            ),
+            statBars[i + 1],
+          ],
+        ));
+      }
+    }
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: statRows,
+    );
   }
 }
